69a9677a2453bac6f3eb26e3defa14e24b6f497b
jeremymordecai628 <jeremymordecai08@gmail.com>
2024-11-20 17:21:26 +0300

    update basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,19 @@
+#!/usr/bin/env python3
+"""
+The class will handle operations for basic authetication
+"""
 from api.v1.auth.auth import Auth
-"""
-The class will remain empty for the moment
-"""
+import base64
+
 class BasicAuth(Auth):
     """Basic authentication class."""
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extract Base64 part from the Authorization header."""
+        if not authorization_header or not isinstance(authorization_header, str) :
+            return None
+        
+        if not authorization_header.startswith('Basic'):
+            return None
+        
+        return authorization_header.split(' ', 1)[1]
+        
