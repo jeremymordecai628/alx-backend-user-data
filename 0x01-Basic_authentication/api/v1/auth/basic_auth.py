214b5275192906c5183d5faf3543b276c4591478
jeremymordecai628 <jeremymordecai08@gmail.com>
2024-11-20 17:32:22 +0300

    update basic auth base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -16,4 +16,17 @@ class BasicAuth(Auth):
             return None
         
         return authorization_header.split(' ', 1)[1]
+    
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        Decodes a Base64 authorization header.
+        """
+        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
+            return None
         
+        try :
+            decoded = base64.b64decode(base64_authorization_header).decode('utf-8')
+            return decoded
+        except Exception:
+            return None
