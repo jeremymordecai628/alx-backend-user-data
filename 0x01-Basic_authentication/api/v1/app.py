2cf61f69119e8bff34f55eba35882f8ac5778d74
jeremymordecai628 <jeremymordecai08@gmail.com>
2024-11-20 10:40:48 +0300

    auth created

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,11 +14,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Unauthorized"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error):
---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,15 @@
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if authentication is required."""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Retrieve Authorization header."""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieve current user."""
+        return None
