9938cc6be3b2ef1f67c5e1685b75c94daee9c6f0
jeremymordecai628 <jeremymordecai08@gmail.com>
2024-11-20 17:43:33 +0300

    update basic auth base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,8 @@ The class will handle operations for basic authetication
 """
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple , TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -43,3 +44,22 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+    
+    
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Retrieves a User instance based on email and password.
+        """
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
